f09ba23df34c0ad08c7b51f988959bab10c84bf2
Caitlin Petro <cpetro@uncc.edu>
2021-07-07 15:20:44 -0400

    Update convert_to_time_cycle.py

--- Code/Data Analysis/convert_to_time_cycle.py
+++ Code/Data Analysis/convert_to_time_cycle.py
@@ -2,34 +2,43 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-def convert_to_time_cycle(read_file_path):
-    df = pd.read_csv(read_file_path, sep=',')
-
-    times_in_seconds = pd.Series(pd.to_timedelta(df[df.columns[0]]))
-    # Convert time to seconds
+   
+def convert_to_time_cycle(file_path):
+    df = pd.read_csv(file_path)
+    times_in_seconds = pd.Series(pd.to_timedelta(df[df.columns[0]]))     # Convert time to seconds
     times_in_seconds = times_in_seconds.dt.total_seconds()
-    # Start time at 0 seconds
-    times_in_seconds = times_in_seconds-min(times_in_seconds)
-    # Scale all times between 0 and 1
-    times_in_seconds = times_in_seconds / max(times_in_seconds)
-    # Our simulations represent a 15-hour period which start at 7am
-    times_in_seconds = times_in_seconds * 15 * 60 * 60 + 7*60*60
-    day = 24*60*60
-    second_to_hour = 60*60
+    times_in_seconds = times_in_seconds - min(times_in_seconds)          # Start time at 0 seconds
+    times_in_seconds = times_in_seconds / max(times_in_seconds)          # Scale all times between 0 and 1
+    times_in_seconds = times_in_seconds * 15 * 60 * 60 + 7 * 60 * 60     # Our simulations represent a 15-hour period which start at 7am
 
-    # Convert times in seconds to sin for LSTM parameter
-    df['Days sin'] = np.sin(times_in_seconds * (2*np.pi / day))
-    # Convert times in seconds to cos for LSTM parameter
-    df['Days cos'] = np.cos(times_in_seconds * (2*np.pi / day))
-
-    # Plotting
-    plt.plot(times_in_seconds/second_to_hour, df['Days sin'])
-    plt.plot(times_in_seconds/second_to_hour, df['Days cos'])
-    plt.xlabel('Hours')
-    plt.show()
+    day = 24 * 60 * 60
+    second_to_hour = 60 * 60
+    x_tick_list = []
+    x_tick_locater = []
+    for i in range(int(min(times_in_seconds / second_to_hour)), int(max(times_in_seconds / second_to_hour))+1):
+        if i<12:
+            x_tick_list.append(f'{i} a.m.')
+        elif i==12:
+            x_tick_list.append(f'{i} p.m.')
+        else:
+            x_tick_list.append(f'{i-12} p.m.')
+        x_tick_locater.append(i)
+    x_fornatter = FixedFormatter(x_tick_list)
+    x_locator = FixedLocator(x_tick_locater)
+    df['Days sin'] = np.sin(times_in_seconds * (2 * np.pi / day))    # Convert times in seconds to sin for LSTM parameter
+    df['Days cos'] = np.cos(times_in_seconds * (2 * np.pi / day))    # Convert times in seconds to cos for LSTM parameter
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.plot(times_in_seconds / second_to_hour, df['Days sin'])
+    ax.plot(times_in_seconds / second_to_hour, df['Days cos'])
+    ax.xaxis.set_major_formatter(x_fornatter)
+    ax.xaxis.set_major_locator(x_locator)
+    ax.tick_params(axis='x', labelrotation=-60)
+    ax.set_xlabel('Time Being Modeled')
+    ax.set_ylabel('Converted Time')
+    ax.set_title('Cyclic Time Representation')
+    fig.savefig('time.png', bbox_inches='tight')
     print(times_in_seconds.head)
-
 
 r_file = input("Input read file: ")
 r_file = r_file.replace('\\', '/')
